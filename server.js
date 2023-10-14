c971ad40950366a8402f9503453ef1e7681bd3ce
Ajay Walke <84366358+AjayWalke@users.noreply.github.com>
2023-10-14 15:04:54 +0530

    First Commit - Backend

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const colors = require('colors');
+const morgan = require("morgan");
+const connectdb = require("./config/db");
+
+dotenv.config({path: './utils/.env'});
+
+const app = new express();
+connectdb();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 8080;
+
+app.use('/api/v1/test', require('./controllers/testcontrollers'));
+app.use('/api/v1/student', require('./routers/studentRoute'));
+app.use('/api/v1/view', require('./routers/viewRoute'));
+app.use('/api/v1/email', require('./routers/emailRoute'));
+
+app.listen(PORT, () => {
+    console.log(`App is listening at port ${PORT} in mode ${process.env.DEV_MODE}`.bgBlue.white);
+});
